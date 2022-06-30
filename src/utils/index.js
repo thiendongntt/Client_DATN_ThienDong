1cf70f2ac8fce168973071c02632ffde826b717b
Hoang Dinh Thien Dong <donghdt@mor.com.vn>
2022-07-01 00:12:42 +0700

    Thien Dong

---
+++ src/utils/index.js
@@ -0,0 +1,24 @@
+export const formatNumber = (x) => {
+  if (x && x !== "null" && x !== "undefined") {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  } else {
+    return 0;
+  }
+};
+
+export const getSalePrice = (price, percent) => {
+  return Math.ceil((price / 100) * (100 - percent));
+};
+
+export const splitFullName = (fullName) => {
+  const partials = fullName.split(" ");
+
+  return {
+    last_name: partials.pop(),
+    first_name: partials.join(" "),
+  };
+};
+
+export const formatPrice = (value) => {
+  return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+}
